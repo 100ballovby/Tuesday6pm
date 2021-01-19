02f5a37fa8c8bbcfb6fe6f017b45514aff003980
demidraksin <greatraksin@icloud.com>
2021-01-19 18:35:50 +0300

    Adding pandas tutorial and dataset

--- 1901/main.py
+++ 1901/main.py
@@ -1,1 +1,10 @@
+import requests
+
 url = 'https://raw.githubusercontent.com/fivethirtyeight/data/master/nba-elo/nbaallelo.csv'
+target_csv = 'nba_all_elo.csv'
+
+response = requests.get(url)
+response.raise_for_status()
+with open(target_csv, 'wb') as f:
+    f.write(response.content)
+print('Downloaded!')
